6a860b0e795c58379577493607f2f1ce9c0b1527
bponedev <bponedev@gmail.com>
2026-01-16 13:01:04 -0300

    Refactor User and Office models and add AuditLog

--- models/models.py
+++ models/models.py
@@ -1,49 +1,41 @@
-from flask_login import UserMixin
-from app import db, login_manager
-
 from database import db
-
-class Office(db.Model):
-    __tablename__ = 'offices'
-
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(20), unique=True, nullable=False)
-    name = db.Column(db.String(120), nullable=False)
-    active = db.Column(db.Boolean, default=True)
-
-    clients = db.relationship('Client', backref='office', lazy=True)
-
-    def __repr__(self):
-        return f'<Office {self.code} - {self.name}>'
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(200))
+    role = db.Column(db.String(20), default='OPERADOR')
+    active = db.Column(db.Boolean, default=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Office(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    active = db.Column(db.Boolean, default=True)
 
 
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    cpf = db.Column(db.String(20))
+    office_id = db.Column(db.Integer, db.ForeignKey('office.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    deleted = db.Column(db.Boolean, default=False)
 
-    nome = db.Column(db.String(120), nullable=False)
-    cpf = db.Column(db.String(20), nullable=False)
 
-    escritorio = db.Column(db.String(120), nullable=False)
-    tipo_acao = db.Column(db.String(120))
-
-    data_contrato = db.Column(db.String(10))
-    data_protocolo = db.Column(db.String(10))
-
-    numero_processo = db.Column(db.String(120))
-    pendencias = db.Column(db.Text)
-    observacoes = db.Column(db.Text)
-
-    captador = db.Column(db.String(120))
-    captador_pago = db.Column(db.String(20))
-
-    excluido = db.Column(db.Boolean, default=False)
+class AuditLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(50))
+    action = db.Column(db.String(200))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
