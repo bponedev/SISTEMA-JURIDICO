1952c987a2afcced968b914c7aaf7ddc0d2dcd36
bponedev <bponedev@gmail.com>
2026-01-16 09:29:41 -0300

    Add authentication routes for login and logout
    Implemented login and logout routes for authentication.

---
+++ routes/auth.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, login_required
+from models.models import User
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and user.password == request.form['password']:
+            login_user(user)
+            return redirect(url_for('clients.form'))
+    return render_template('login.html')
+
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
