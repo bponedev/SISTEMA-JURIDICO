aef2238022526e638ce318958df5e85022c04903
bponedev <bponedev@gmail.com>
2026-01-16 13:00:23 -0300

    Refactor app.py to streamline app initialization

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from database import db
+from models.models import User
+from routes.auth import auth_bp
+from routes.clients import clients_bp
+from routes.admin import admin_bp
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-def create_app():
-    app = Flask(__name__)
+db.init_app(app)
 
-    app.config['SECRET_KEY'] = 'dev-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+login_manager = LoginManager(app)
+login_manager.login_view = 'auth.login'
 
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-    from routes.auth import auth_bp
-    from routes.clients import clients_bp
+with app.app_context():
+    db.create_all()
+    if not User.query.filter_by(username='admin').first():
+        admin = User(username='admin', name='ADMIN', role='ADMIN')
+        admin.set_password('123')
+        db.session.add(admin)
+        db.session.commit()
 
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(clients_bp)
-
-    with app.app_context():
-        db.create_all()
-        from models.models import User
-        if not User.query.filter_by(username='admin').first():
-            admin = User(username='admin', password='admin')
-            db.session.add(admin)
-            db.session.commit()
-
-    return app
-
-app = create_app()
+app.register_blueprint(auth_bp)
+app.register_blueprint(clients_bp)
+app.register_blueprint(admin_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
